90295d669d6f99c33860e3390a3f3ae034814b71
Pratham-The-Warrior <rockxyz70@gmail.com>
2025-05-27 15:42:53 +0530

    Add files via upload
    A simple quiz just like google forms made with html,css,js

---
+++ 3-QuizApp/script2.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      answer: "Paris"
+    },
+    {
+      question: "Who is known as the 'Father of the Nation' in India?",
+      options: ["Subhas Chandra Bose", "Mahatma Gandhi", "Jawaharlal Nehru", "B. R. Ambedkar"],
+      answer: "Mahatma Gandhi"
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Venus", "Mars", "Jupiter"],
+      answer: "Mars"
+    },
+    {
+      question: "How many continents are there in the world?",
+      options: ["5", "6", "7", "8"],
+      answer: "7"
+    },
+    {
+      question: "Who wrote the national anthem of India?",
+      options: ["Rabindranath Tagore", "Bankim Chandra Chatterjee", "Sarojini Naidu", "C. Rajagopalachari"],
+      answer: "Rabindranath Tagore"
+    },
+    {
+      question: "What is the largest ocean in the world?",
+      options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+      answer: "Pacific Ocean"
+    },
+    {
+      question: "In which year did India become independent?",
+      options: ["1945", "1946", "1947", "1948"],
+      answer: "1947"
+    }
+  ];
+  
+  
